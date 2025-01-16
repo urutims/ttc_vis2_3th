e2853f9113822e274ffecb77e0edaac1f6c6218f
urutims <mw24366015@ga.ttc.ac.jp>
2025-01-16 14:51:15 +0900

    testcommit

--- app.py
+++ app.py
@@ -14,11 +14,11 @@ st.subheader('確率分布の実験')
 母数（パラメータ）を変化させたときのグラフの変化の確認
 '''
 
-## 期待値と分散を指定
+# 期待値と分散を指定
 mu = st.sidebar.slider('正規分布の期待値', min_value=-5.0, max_value=5.0, step=0.01)
 sigma = st.sidebar.slider('正規分布の分散', min_value=0, max_value=20, step=1)
 
-## 標準正規分布の描画
+# 標準正規分布の描画
 x_1 = np.linspace(-10, 10, 100)
 z = stats.norm.pdf(x_1, mu, sigma)
 fig_norm, ax1 = plt.subplots()
@@ -27,3 +27,4 @@ ax1.legend()
 st.pyplot(fig_norm)
 
 
+st.write('testcommitの証拠')
